eb56eb9b2b201a958170acae56ed90bbf0f8cdbe
老房东 <huangdong@gmail.com>
2024-08-29 08:46:04 -0400

    if score >= 20 show winner

--- app.py
+++ app.py
@@ -43,6 +43,12 @@ def game():
     return render_template('game.html', players=players)
 
 
+@app.route('/winner', methods=['GET'])
+def winner():
+    players = session.get('players')
+    return render_template('winner.html', players=players)
+
+
 @app.route('/roll', methods=['GET'])
 def roll():
     players = session.get('players')
@@ -54,6 +60,8 @@ def roll():
     else:
         players[players['current_player'] + '_score'] += players['dice']
     session['players'] = players
+    if players[players['current_player'] + '_score'] >= 20:
+        return render_template('winner.html', players=players)
     return render_template('game_form.html', players=players)
 
 
